73aa93dfd85c42e4a375bddd72cf640f9e9fb7a8
Ramil ♥ <114113891+SupraLove@users.noreply.github.com>
2023-05-01 15:16:48 +0300

    Add files via upload

---
+++ html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "acdegilnoprsuw",
+  1: "acdeosw",
+  2: "a",
+  3: "acdginorsuw",
+  4: "adeiloprs"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы",
+  2: "Файлы",
+  3: "Функции",
+  4: "Переменные"
+};
+
